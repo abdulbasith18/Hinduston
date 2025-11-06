21407f82a7fffaf7102ee248aa956a9058d81f25
Abdul Basith <mailsforbasith@gmail.com>
2025-11-06 11:46:10 +0530

    first commit

---
+++ src/firstfoor/Bca.java
@@ -0,0 +1,20 @@
+package firstfoor;
+
+public class Bca {
+	public void abdul() {
+		System.out.println("First Output");
+	}
+	public void mohamed() {
+		System.out.println("second Output");
+	}
+	public void basith() {
+		System.out.println("third Output");
+	}
+	
+public static void main(String[]args) {
+	Bca a = new Bca();
+	a.abdul();
+	a.basith();
+	a.mohamed();
+}
+}
